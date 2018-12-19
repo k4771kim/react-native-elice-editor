fe45159d16d95aa9984b535029a654595364a786
Kim Bogyun <bogyun@elicer.com>
2018-12-19 16:33:16 +0900

    delete EliceEditorProps in index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ import React from 'react'
 import { requireNativeComponent, TextInputProps } from 'react-native'
 import PropTypes from 'prop-types'
 
-var EliceEditorProps = {
-  name: 'RNEliceEditor',
-  propTypes: {
-    ...TextInputProps
-  }
-}
-
-const RNEliceEditor = requireNativeComponent('RNEliceEditor', EliceEditorProps)
+const RNEliceEditor = requireNativeComponent('RNEliceEditor')
 
 export default RNEliceEditor
