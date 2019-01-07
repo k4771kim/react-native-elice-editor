a87bdce2aa0dc3e047da6eaa158721f26f901a59
Kim Bogyun <bogyun@elicer.com>
2019-01-07 13:59:17 +0900

    add android elice-editor

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import React from 'react'
-import { requireNativeComponent, TextInputProps } from 'react-native'
+import React, { cloneElement, Component } from 'react'
+import {
+  TextInput,
+  UIManager,
+  requireNativeComponent,
+  TouchableWithoutFeedback,
+  TextInputProps
+} from 'react-native'
 import PropTypes from 'prop-types'
-
 const RNEliceEditor = requireNativeComponent('RNEliceEditor')
-
-export default RNEliceEditor
+const textInputRef = React.createRef()
+export default class extends Component {
+  render () {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          this.refs.textInputRef.focus()
+        }}
+      >
+        <RNEliceEditor {...this.props} ref='textInputRef' />
+      </TouchableWithoutFeedback>
+    )
+  }
+}
