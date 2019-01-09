31caf5f152cb4e78fd78d61b633b352e432b4268
Kim Bogyun <bogyun@elicer.com>
2019-01-09 17:00:22 +0900

    update color

--- src/EsprimaHelper.js
+++ src/EsprimaHelper.js
@@ -1,9 +1,9 @@
 exports.Colors = {
   Default: 'black',
   Error: 'red',
-  Keyword: '#E7CE56',
-  Identifier: '#EE7C2B',
-  Punctuator: 'black',
-  Numeric: '#EB3C54',
-  String: '#B6B18B'
+  Keyword: '#4286f4',
+  Identifier: '#aaaaaa',
+  Punctuator: 'ac1960',
+  Numeric: '#1fc96a',
+  String: '#fecb45'
 }
