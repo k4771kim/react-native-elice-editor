8450e4680afabec137cae23c2a400adfc14ccd03
Kim Bogyun <bogyun@elicer.com>
2019-01-09 16:27:17 +0900

    customize for elice mobile

--- src/EliceEditor.js
+++ src/EliceEditor.js
@@ -12,9 +12,7 @@ const styles = {
   inputWrapper: {
     position: 'absolute',
     top: 0,
-    width: '100%',
-    borderWidth: 1,
-    borderColor: 'gray'
+    width: '100%'
   },
   text: {
     position: 'absolute',
@@ -31,13 +29,57 @@ export default class EliceEditor extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      inputText: '',
       formattedText: ''
     }
+    this._lastNativeSelection = {
+      start: 0,
+      end: 0
+    }
   }
-  _lastNativeSelection = {
-    start: 0,
-    end: 0
+  init = false
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.defaultValue !== nextProps.defaultValue) {
+      this.initiate(this.props.defaultValue)
+      this.init = true
+    }
+  }
+
+  initiate (inputText) {
+    let lastLoc = 0
+    const formattedText = []
+    let tokenizedText = esprima.tokenize(inputText, {
+      tolerant: true,
+      range: true,
+      loc: true,
+      comment: true
+    })
+    tokenizedText.forEach(object => {
+      if (lastLoc !== object.loc.start.column) {
+        const errorText = (
+          <Text style={{ color: Colors.Error }}>
+            {inputText.slice(lastLoc, object.loc.start.column)}
+          </Text>
+        )
+        formattedText.push(errorText)
+      }
+      const mention = (
+        <Text style={{ color: Colors[object.type] }}>{object.value}</Text>
+      )
+      formattedText.push(mention)
+      lastLoc = object.loc.end.column
+    })
+
+    if (lastLoc !== inputText.length) {
+      const errorText = (
+        <Text style={{ color: Colors.Error }}>
+          {inputText.slice(lastLoc, inputText.length)}
+        </Text>
+      )
+      formattedText.push(errorText)
+    }
+
+    this.setState({ formattedText })
   }
   onSelectionChange (event) {
     this.props.onSelectionChangeEvent &&
@@ -77,7 +119,7 @@ export default class EliceEditor extends React.Component {
       )
       formattedText.push(errorText)
     }
-    this.setState({ inputText, formattedText })
+    this.setState({ formattedText })
 
     this.props.onChangeTextEvent && this.props.onChangeTextEvent(inputText)
   }
@@ -103,7 +145,6 @@ export default class EliceEditor extends React.Component {
               this.props.style,
               { backgroundColor: 'transparent' }
             ]}
-            value={this.state.inputText}
             onSelectionChange={this.onSelectionChange.bind(this)}
             onChangeText={this.handleChangeText}
             autoCapitalize='none'
